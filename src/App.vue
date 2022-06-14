42c05a03cb76e39086c059b7c698f1b4c308d7db
Victoria Trachuk <victoriatrachuk@MacBook-Pro-Victoria.local>
2022-06-14 05:54:34 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <masonry-wall
+    :items="getList"
+    :ssr-columns="2"
+    :column-width="setColWidth"
+    :gap="gap"
+  >
+    <template #default="{ item, index }">
+      <img :src="item.download_url" />
+      <div class="content">
+        <h5 class="author">{{ index + 1 }}. {{ item.author }}</h5>
+        <p class="id">Id: {{ item.id }}</p>
+      </div>
+    </template>
+  </masonry-wall>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const axios = require("axios").default;
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      windowWidth: null,
+      getList: [],
+      containerId: 1,
+      gap: 16,
+      page: 1,
+    };
+  },
+  computed: {
+    setColWidth() {
+      const w = this.windowWidth / 2 - this.gap * 3;
+      return w;
+    },
+  },
+  created() {
+    this.windowWidth = window.innerWidth;
+    this.getItems();
+    window.addEventListener("resize", this.resizeListener);
+    window.addEventListener("scroll", this.addScrollListener);
+  },
+  beforeUnmount() {
+    window.removeEventListener("resize", this.resizeListener);
+    window.removeEventListener("scroll", this.addScrollListener);
+  },
+  methods: {
+    resizeListener() {
+      this.windowWidth = window.innerWidth;
+    },
+    addScrollListener() {
+      const docEl = document.documentElement;
+      if (!docEl.scrollTop) return;
+      if (docEl.scrollTop + window.innerHeight >= docEl.scrollHeight - 100) {
+        this.page += 1;
+        this.getItems();
+      }
+    },
+    async getItems() {
+      const limit = 10;
+      await axios
+        .get(`https://picsum.photos/v2/list?page=${this.page}&limit=${limit}`)
+        .then((response) => {
+          const existList = [...this.getList];
+          const newList = [...response.data];
+          this.getList = existList.concat(newList);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "~@/assets/style/main.scss";
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import MasonryWall from '@yeger/vue-masonry-wall'
+
+let app = createApp(App)
+app.use(VueAxios, axios);
+app.use(MasonryWall)
+app.mount('#app')
+
